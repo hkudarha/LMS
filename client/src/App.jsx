b424bea8073adabac479f5a3b7c08d8f51be3c5f
hkudarha <hkudarha@gmail.com>
2025-02-25 17:12:36 +0530

    c

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      
+      <Button className="bg-red-500">hello</Button>
+      
+    </>
+  )
+}
+
+export default App
