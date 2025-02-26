c06291c3277f0ed1f819c803bdcb1b3f6e0b3c33
hkudarha <hkudarha@gmail.com>
2025-02-26 14:59:54 +0530

    form

---
+++ client/src/config/index.js
@@ -0,0 +1,23 @@
+export const signUpFormControls = [
+    {
+        name :'username',
+        label : 'User Name',
+        placeholder : 'Enter your user name',
+        type : 'text',
+        componentType : 'input'
+    },
+    {
+        name :'email',
+        label : 'User Email',
+        placeholder : 'Enter your user emaile',
+        type : 'text',
+        componentType : 'input'
+    },
+    {
+        name :'password',
+        label : 'Password',
+        placeholder : 'Enter your Password',
+        type : 'password',
+        componentType : 'input'
+    },
+]
--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,7 +1,47 @@
+import CommonForm from "@/components/common-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { signUpFormControls } from "@/config";
+import { GraduationCap } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function AuthPage() {
+
+  const [activeTab, setActiveTab] = useState('signin');
+
+  function handleTabChange(value){
+    setActiveTab(value)
+  }
+
   return (  
-    <div>
-      <h1>hello</h1>
+    <div className="flex flex-col min-h-screen">
+      <header className="px-4 lg:px-6 h-14  flex items-center border-b">
+        <Link to={'/'} className="flex items-center justify-center ">
+          <GraduationCap className="h-8 w-8 mr-4 "/>
+          <span className="font-extrabold text-xl ">LMS LEARN</span>
+        </Link>
+      </header>
+
+      <div className="flex items-center justify-center min-h-screen bg-background ">
+        <Tabs
+        value={activeTab}
+        defaultValue="signin"
+        onValueChange={handleTabChange}
+        className="w-full max-w-md" 
+        >
+          <TabsList className='grid w-full grid-cols-2'>
+            <TabsTrigger value='signin'>Sign In</TabsTrigger>
+            <TabsTrigger value='signup'>Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value='signin'>
+            signIn
+          </TabsContent>
+          <TabsContent value='signup'>
+            <CommonForm formControls={signUpFormControls}/>
+            
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
